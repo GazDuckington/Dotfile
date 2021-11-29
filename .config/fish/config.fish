23764fdc28359d952d6655f2b68f78d0a5f75e3c
RegalOctopus <dianghazy@protonmail.com>
2021-11-29 17:49:22 +0700

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,15 +6,22 @@ set EDITOR nvim $EDITOR
 
 alias 'br'='~/scripts/vr'
 alias 'ls'='lsd'
+alias 'la'='lsd -a'
 alias 'll'='lsd -l'
 alias 'tree'='lsd --tree'
 alias 'vim'='nvim'
+alias 'lv'='lvim'
 alias 'code'='codium'
+alias 'c'='codium'
 
 if status --is-interactive
   if test -z "$DISPLAY" -a $XDG_VTNR = 1
     exec startx -- -keeptty
   end
+end
+
+function lolban
+  figlet -f ansi-shadow $argv | lolcat
 end
 
 starship init fish | source

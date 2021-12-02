e850f934a6254ef1bd9e92e07240b9bec7589f94
RegalOctopus <dianghazy@protonmail.com>
2021-12-02 22:43:04 +0700

    update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -13,6 +13,14 @@ alias 'vim'='nvim'
 alias 'lv'='lvim'
 alias 'code'='codium'
 alias 'c'='codium'
+alias 'ga'='git add'
+alias 'gc'='git commit'
+alias 'gp'='git push'
+alias 'i'='yay -S --noconfirm'
+alias 's'='yay -Ss'
+alias 'f'='yay -Si'
+alias 'r'='yay -Rs --noconfirm'
+alias 'ar'='yay -Rs (yay -Qtdq)'
 
 if status --is-interactive
   if test -z "$DISPLAY" -a $XDG_VTNR = 1

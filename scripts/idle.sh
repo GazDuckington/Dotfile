a3c7082411c16cb766ba548f1104a4e2860f5295
RegalOctopus <dianghazy@protonmail.com>
2021-12-12 19:16:10 +0700

    update

--- scripts/idle.sh
+++ scripts/idle.sh
@@ -4,7 +4,7 @@ xidlehook \
   --not-when-fullscreen \
   --not-when-audio \
   --timer 600 \
-    '~/scripts/i3lock' \
+    '~/scripts/i3lock.sh' \
     ''\
   --timer 1200 \
     'systemctl suspend' \

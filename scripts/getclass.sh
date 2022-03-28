91a0b5c9b7503c3ec44fc81549c8b5db6b8a6a07
gaz <dianghazy@protonmail.com>
2022-03-28 21:05:03 +0700

    update: getclass.sh

--- scripts/getclass.sh
+++ scripts/getclass.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-title=$(xprop -id $(xdotool getwindowfocus) WM_CLASS | awk '{sub("\"","");sub("\",","");printf $3}')
+title=$(xprop -id $(xdotool getwindowfocus) WM_CLASS | awk '{sub(/"/,"",$4);sub(/"$/,"");print $4}')
 
-if [[ $title == "found." ]]; then
+if [[ -z $title ]]; then
 	echo $USER
 else
 	echo $title

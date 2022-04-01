a63f939b78db687c5844de0619de9a45aab483d8
gaz <dianghazy@protonmail.com>
2022-04-02 04:38:38 +0700

    add: dunst-volume.sh

---
+++ scripts/disableETPS.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+xinput set-prop "ETPS/2 Elantech TrackPoint" "Device Enabled" 0
+xinput set-prop "ETPS/2 Elantech Touchpad" "Device Enabled" 0
+xinput set-prop "AT Translated Set 2 keyboard" "Device Enabled" 0

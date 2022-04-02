596d7a2d28d92889681f8658d69323c5eabf5857
gaz <dianghazy@protonmail.com>
2022-04-02 17:47:14 +0700

    update

--- scripts/playercontrol.sh
+++ scripts/playercontrol.sh
@@ -6,6 +6,7 @@ zscroll -l 20 \
 	-M "playerctl status --player=$cube" \
         -m "Playing" "-b ' '" \
         -m "Paused" "-b ' '" \
+        -m "Paused" "-b ' '" \
 	-m "No\ players\ found" "-b '𥉉 '" \
         -u true "playerctl --player=$cube metadata --format '{{artist}}-{{title}}'" &
 

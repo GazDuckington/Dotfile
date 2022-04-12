142b5148ae15a7585cd5ca493c3e5ff656ce19fa
Gaz <dianghazy@protonmail.com>
2022-04-13 06:50:02 +0700

    update

--- config/rofi/scripts/dev.sh
+++ config/rofi/scripts/dev.sh
@@ -3,12 +3,12 @@
 rofi_command="rofi -i -matching fuzzy"
 
 # Variable passed to rofi
-options="$(ls ~/dev/)"
+options="$(ls ~/Dev/)"
 
 chosen="$(echo -e "$options" | $rofi_command -p 'Select' -dmenu)"
 
 if ["$chosen" = ""]; then
 $chosen
 else
-code-insiders ~/dev/$chosen
+code-insiders ~/Dev/$chosen
 fi

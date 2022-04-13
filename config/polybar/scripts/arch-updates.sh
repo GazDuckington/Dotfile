24f80b18e72b37ac2f8ecc2e5344bc023ffafa69
Gaz <dianghazy@protonmail.com>
2022-04-14 04:30:37 +0700

    update: arch update script

--- config/polybar/scripts/arch-updates.sh
+++ config/polybar/scripts/arch-updates.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-updates=$(checkupdates+aur 2> /dev/null | wc -l)
+if ! updates=$(checkupdates+aur 2> /dev/null | wc -l); then
+   updates=0
+fi
 
-if [ "$updates" -gt 0 ]; then
-   echo "%{F#50fa7b} $updates%{F-}"
+if (( $updates > 0 )); then
+   echo "%{F#50fa7b} $updates%{F-}"
+elif (( $updates >= 10 )); then
+   echo "%{F#ff5455} $updates%{F-}"
 else
-   echo "%{F#6272a4} $updates%{F-}"
+   echo "%{F#6272a4} $updates%{F-}"
 fi

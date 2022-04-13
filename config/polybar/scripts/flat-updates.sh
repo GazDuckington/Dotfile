338173444074064db897216303a26a9f17ce6e2b
Gaz <dianghazy@protonmail.com>
2022-04-14 05:13:40 +0700

    update

--- config/polybar/scripts/flat-updates.sh
+++ config/polybar/scripts/flat-updates.sh
@@ -3,7 +3,7 @@
 updates=$(echo 'n' | flatpak update 2>/dev/null | tail -n +5 | head -2 | wc -l)
 
 if [ "$updates" -gt 0 ]; then
-    echo "%{F#50fa7b} $updates%{F-}"
+    echo "%{F#50fa7b}$updates%{F-}"
 else
-    echo "%{F#6272a4} $updates%{F-}"
+    echo "%{F#6272a4}$updates%{F-}"
 fi

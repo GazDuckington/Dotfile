c6dcb2066ed977376565daf39a3884509455dc82
Gaz <dianghazy@protonmail.com>
2022-05-24 17:53:47 +0700

    battery watch

--- scripts/battery-watch.sh
+++ scripts/battery-watch.sh
@@ -1,32 +1,10 @@
-# #! /usr/bin/env bash
-
-# if ! status=$(cat /sys/class/power_supply/BAT0/capacity 2> /dev/null); then
-#     status=""
-# fi
-
-# if (( $status <= 30 )); then
-#     dunstify -u critical -t 10000 " LOW BATTERY! 
-# BATTERY AT: $status%" -a "L530"
-# fi
-
-#!/bin/sh
-
-CAPACITY=$(cat /sys/class/power_supply/BAT0/capacity)
-
-STATUS=$(cat /sys/class/power_supply/BAT0/status)
-
-[ "$STATUS" = "Discharging" ] && [ "$CAPACITY" -lt 40 ] && \
-
-dunstify -u critical "Low Battery" "You are running low on battery ($CAPACITY%).\nPlease plug in the charger." && \
-
-echo "Low Battery notification sent" ||\
-
-echo "Low Battery notification not sent"
-
-[ "$STATUS" = "Charging" ] && [ "$CAPACITY" -gt 97 ] && \
-
-dunstify -u normal "Battery Full" "Charge full ($CAPACITY%).\nPlease unplug from charger." && \
-
-echo "Full Battery notification sent" ||\
-
-echo "Full Battery notification not sent"
+#!/usr/bin/env bash
+BATPATH=/sys/class/power_supply/BAT0
+BAT_FULL=$BATPATH/energy_full
+BAT_NOW=$BATPATH/energy_now
+bf=$(cat $BAT_FULL)
+bn=$(cat $BAT_NOW)
+if [ $(( 100 * $bn / $bf )) -lt 60 ]
+then
+    dunstify -u critical -t 8000 "low battery!"
+fi

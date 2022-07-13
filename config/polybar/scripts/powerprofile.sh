4c7a57548b300c1c9bd110985ecc9a15c75e923e
Gaz <dianghazy@protonmail.com>
2022-07-13 17:21:20 +0700

    update

--- config/polybar/scripts/powerprofile.sh
+++ config/polybar/scripts/powerprofile.sh
@@ -4,7 +4,7 @@ PROFILE=$(powerprofilesctl get)
 if [ $PROFILE == "balanced" ]; then
     echo "%{F#d2daf4}%{F-}"
 elif [ $PROFILE == "performance" ]; then
-    echo "%{F#daa640}%{F-}"
+    echo "%{F#daa640}%{F-}"
 elif [ $PROFILE == "power-saver" ]; then
     echo "%{F#53db7f}%{F-}"
 fi

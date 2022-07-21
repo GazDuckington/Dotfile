507113da1fe67b029ee349b6fb7556d83b837353
Gaz <dianghazy@protonmail.com>
2022-07-21 16:49:25 +0700

    update

---
+++ config/dktheme/themes/github/github-color.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+bg='#0d1117'
+fg='#c9d1d9'
+indigo='#6272a4'
+grey='#30363d'
+green='#3fb950'
+cyan='#51d5c3'
+purple='#a371f7'
+red='#f85149'
+yellow='#d29922'
+blue='#58a6ff'
+teal='#79c0ff'
+bluegray='#b3b9b8'

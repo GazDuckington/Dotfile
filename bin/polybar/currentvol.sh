ff88cf19ddb11053549ec461c9f6ecf51019b747
Gaz <dianghazy@protonmail.com>
2022-11-03 17:03:00 +0700

    stuff

---
+++ bin/polybar/currentvol.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+. ~/.config/dk/color.sh
+vol="$(amixer get Master | tail -1 | awk '{print $5}' | sed 's/[^0-9]*//g')"
+mute="$(amixer get Master | tail -1 | awk '{print $6}' | sed 's/[^a-z]*//g')"
+
+if [[ $mute == "off" ]]; then
+	echo "%{F$mgrey}ﱝ muted%{F-}"
+elif [[ $vol = 0 ]];then
+	echo "%{F$flamingo}奄 ${vol}%%{F-}"
+elif [[ $vol > 50 || $vol = 100 ]]; then
+	echo "%{F$flamingo}墳 ${vol}%%{F-}"
+else
+	echo "%{F$flamingo}奔 ${vol}%%{F-}"
+fi

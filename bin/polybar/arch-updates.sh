e16641f563ee2d1748ef75d31879774f5124d19d
Gaz <dianghazy@protonmail.com>
2022-11-03 17:11:45 +0700

    stuff

--- bin/polybar/arch-updates.sh
+++ bin/polybar/arch-updates.sh
@@ -1,24 +1,15 @@
 #!/bin/sh
 
-if ! updates=$(checkupdates 2>/dev/null | wc -l); then
-   updates=0
-fi
-
-echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 >/dev/null 2>&1
-
 . ~/.config/dk/color.sh
 
-if [ $? -eq 0 ]; then
+if ! updates=$(yay -Qum 2> /dev/null | wc -l); then
+    updates=0
+fi
 
-   if (($updates == 0)); then
-      # echo "%{F$green}﫟%{F-}"
-			echo ""
-   elif (($updates > 50)); then
-      echo "%{F$red} %{F-}$updates"
-   else
-      echo "%{F$yellow} %{F-}$updates"
-   fi
-
+if [ "$updates" -gt 0 ]; then
+  echo "%{F$green} $updates%{F-}"
+elif [ "$updates" -gt 50 ];then
+	echo "%{F$red} $updates%{F-}"
 else
-   echo "%{F$grey}%{F-}"
+    echo ""
 fi
--- bin/polybar/flat-updates.sh
+++ bin/polybar/flat-updates.sh
@@ -1,20 +1,12 @@
 #!/bin/sh
 
+. ~/.config/dk/color.sh
+
 updates=$(echo 'n' | flatpak update 2>/dev/null | tail -n +5 | head -2 | wc -l)
 
-echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 >/dev/null 2>&1
-. ~/.config/dk/color.sh
-if [ $? -eq 0 ]; then
-
-    if (($updates == 0)); then
-        # echo "%{F$green}%{F-}"
-				echo ""
-    elif (($updates > 10)); then
-        echo "%{F$red} %{F-}$updates"
-    else
-        echo "%{F$yellow} %{F-}$updates"
-    fi
-
+if (($updates == 0)); then
+		echo ""
 else
-    echo "%{F$grey}%{F-}"
+    echo "%{F$green} %{F-}$updates"
 fi
+

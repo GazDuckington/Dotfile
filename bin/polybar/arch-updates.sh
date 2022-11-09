ce204fa3fb2bc56faaa7ae97fd3860ffef52c337
Gaz <dianghazy@protonmail.com>
2022-11-09 16:43:28 +0700

    arch update notifier

--- bin/polybar/arch-updates.sh
+++ bin/polybar/arch-updates.sh
@@ -2,14 +2,20 @@
 
 . ~/.config/dk/color.sh
 
-if ! updates=$(yay -Qum 2> /dev/null | wc -l); then
-    updates=0
+if ! updates_arch=$(checkupdates 2> /dev/null | wc -l); then
+	updates_arch=0
 fi
+
+if ! updates_aur=$(yay -Qua 2> /dev/null | wc -l); then
+	updates_aur=0
+fi
+
+updates=$((updates_arch + updates_aur))
 
 if [ "$updates" -gt 0 ]; then
   echo "%{F$green} $updates%{F-}"
 elif [ "$updates" -gt 50 ];then
 	echo "%{F$red} $updates%{F-}"
 else
-    echo ""
+  echo ""
 fi

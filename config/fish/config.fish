73fd9fadf07a015153b50648a46fdec66bcfa8d5
Gaz <dianghazy@protonmail.com>
2022-11-21 14:50:41 +0700

    up

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -15,6 +15,7 @@ alias 'fzf'='fzf --layout reverse --border rounded --ansi'
 alias 'pm'='pulsemixer'
 alias 'cl'='clear'
 
+alias 'rm'='trash'
 alias 'cpr'='cp -r'
 alias 'ls'='lsd'
 alias 'la'='lsd -a'

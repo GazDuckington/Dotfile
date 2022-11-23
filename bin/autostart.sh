94139a5d0b40e72434636a99cdee4837bbb0ef3a
Gaz <dianghazy@protonmail.com>
2022-11-24 06:26:14 +0700

    bleeegh

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -5,9 +5,9 @@ killall -q xidlehook &
 pgrep -x dunst >/dev/null || dunst &
 /usr/lib/polkit-kde-authentication-agent-1 &
 ~/bin/polybar/launch-polybar.sh main &
-~/bin/idle -s &
-~/bin/disableETPS 0 &
-~/bin/scratchpad dropdown &
+~/bin/idle.sh -s &
+~/bin/disableETPS.sh 0 &
+~/bin/scratchpad.sh dropdown &
 picom --experimental-backends -b &
 pgrep -x com.github.hluk.copyq >/dev/null || com.github.hluk.copyq &
 pgrep -x nm-applet >/dev/null || nm-applet &

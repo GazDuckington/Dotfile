9eb06037345aedc73cceb96d928895429018a706
Gaz <dianghazy@protonmail.com>
2022-11-28 19:59:22 +0700

    update nvim-note

--- bin/nvim-note.sh
+++ bin/nvim-note.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-noteFileName="$HOME/Nextcloud/journals/$(date +%Y-%m-%d).md"
+if [ ! -z $1 ];then
+	rootDir="$HOME/Nextcloud"
+	noteFileName="$rootDir/$1/$(date +%Y-%m-%d).md"
+	mkdir -p $rootDir/$1
 
-if [ ! -f $noteFileName ]; then
-  echo "# Notes of $(date +%d/%B/%Y)" > $noteFileName
+	if [ ! -f $noteFileName ]; then
+	  echo "# Notes of $(date +%d/%B/%Y)" > $noteFileName
+	fi
+
+	nvim -c "normal Go"\
+	     -c "normal Go## $(date +%H:%M)" \
+	     -c "normal G2o" \
+	     -c "normal zz" \
+	     -c "startinsert" $noteFileName \
+	     +"cd %:p:h"
+else
+	echo 'please input directory name'
 fi
-
-nvim -c "normal Go"\
-     -c "normal Go## $(date +%H:%M)" \
-     -c "normal G2o" \
-     -c "normal zz" \
-     -c "startinsert" $noteFileName \
-     +"cd %:p:h"

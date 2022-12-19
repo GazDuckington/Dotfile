43f62b7c856272773e1199d719c2eac2f2c06baf
Ghazy <dianghazy@protonmail.com>
2022-12-19 16:26:59 +0700

    autostart

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -7,9 +7,10 @@ $HOME/bin/polybar/launch-polybar.sh main &
 $HOME/bin/scratchpad.sh dropdown &
 $HOME/bin/disableETPS.sh 0 &
 xsetroot -cursor_name left_ptr &
-#watch -n 60 ~/bin/battery-watch.sh >/dev/null &
+watch -n 60 ~/bin/battery-watch.sh >/dev/null &
 
-# pgrep -x dunst >/dev/null || dunst &
+killall dunst &
+pgrep -x dunst >/dev/null || dunst &
 pgrep -x com.github.hluk.copyq >/dev/null || flatpak run com.github.hluk.copyq &
 pgrep -x nm-applet >/dev/null || nm-applet &
 pgrep -x udiskie >/dev/null || udiskie -t &

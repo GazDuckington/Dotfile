9162dd8d11a1a70ff8f0a7e83615125e5a79ac3f
Ghazy <dianghazy@protonmail.com>
2022-12-29 16:08:23 +0700

    add: dunst.service

--- home/bin/autostart.sh
+++ home/bin/autostart.sh
@@ -8,8 +8,8 @@ pgrep -x xfce-polkit>/dev/null || /usr/libexec/xfce-polkit &
 "$HOME"/bin/disableETPS.sh 0 &
 watch -n 60 ~/bin/battery-watch.sh >/dev/null &
 
-killall dunst &
-pgrep -x dunst >/dev/null || dunst &
+# killall dunst &
+# pgrep -x dunst >/dev/null || dunst &
 pgrep -x com.github.hluk.copyq >/dev/null || flatpak run com.github.hluk.copyq &
 # pgrep -x org.flameshot.Flameshot >/dev/null || flatpak run org.flameshot.Flameshot &
 pgrep -x nm-applet >/dev/null || nm-applet &

2c9054863545ab41e837a23cf5c4aa4a867c5c47
Ghazy <dianghazy@protonmail.com>
2023-01-26 00:52:50 +0700

    add: calendar notification

---
+++ bin/.local/bin/calnotify.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+green="${1:-32CD32}"
+
+day="$(date +'%-d ' | sed 's/\b[0-9]\b/ &/g')"
+cal="$(cal | sed -e 's/^/ /g' -e 's/$/ /g' -e "s/$day/\<span color=\'#$green\'\>\<b\>$day\<\/b\>\<\/span\>/" -e '1d')"
+top="$(cal | sed '1!d')"
+
+dunstify -t 5000 -r 55 "$top" "$cal"

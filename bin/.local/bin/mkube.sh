bf610eb4e3b35c3c87fa282ef5a1467c8421b4c2
Ghazy <dianghazy@protonmail.com>
2023-01-31 14:56:23 +0700

    update: minikube installer

--- bin/.local/bin/mkube.sh
+++ bin/.local/bin/mkube.sh
@@ -1,6 +1,31 @@
 #!/bin/sh
-echo 'downloading minikube binary...'
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-echo 'installing minikube binary'
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
-echo 'donezo!'
+
+sync_version() {
+	echo 'downloading minikube binary...' &&
+	curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 &&
+	echo 'installing minikube binary' &&
+	sudo install minikube-linux-amd64 /usr/local/bin/minikube &&
+	echo 'donezo!'
+}
+
+remove_minikube() {
+	echo 'removing binary' &&
+	sudo rm /usr/local/bin/minikube &&
+	echo 'donezo!'
+}
+
+main() {
+	case "$1" in
+		"(install|update)")
+			sync_version
+		;;
+		"(remove|uninstall)")
+			remove_minikube
+		;;
+		*)
+			sync_version
+		;;
+	esac
+}
+
+main "$1"

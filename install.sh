7b91c508fb0f765040f957e2b4424597458f106b
Ghazy <dianghazy@protonmail.com>
2023-02-15 16:39:49 +0700

    chores

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ignored_files="(.(md|png|sh)|alacritty|[Ff]irefox|zathura|wezterm|glrnvim|vimix|systemd)"
+ignored_files="(.(md|png|sh)|[Ff]irefox|zathura|wezterm|glrnvim|vimix|systemd)"
 dots=$(ls | egrep -v "${ignored_files}")
 for f in $dots;do
 	stow "${f}/"

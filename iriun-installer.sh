314249bcf36a410998974a928d0f6cafe0afa9dc
Ghazy <dianghazy@protonmail.com>
2023-02-15 16:41:05 +0700

    u

--- iriun-installer.sh
+++ iriun-installer.sh
@@ -9,7 +9,7 @@ mkdir ~/Downloads/Iriun
 mv ~/Downloads/iriun.gitlab.io/iriunwebcam-*.deb ~/Downloads/Iriun
 cd ~/Downloads/Iriun && ar vx ~/Downloads/Iriun/iriunwebcam-*.deb
 cd ~/Downloads/Iriun && tar -xf ~/Downloads/Iriun/data.tar.xz
-mv ~/Downloads/Iriun/usr/local/bin/iriunwebcam ~/.local/share/bin/
+mv ~/Downloads/Iriun/usr/local/bin/iriunwebcam ~/.local/bin/
 rm -rf ~/Downloads/Iriun && rm -rf ~/Downloads/iriun.gitlab.io
 
 echo Done, run it by typing iriunwebcam

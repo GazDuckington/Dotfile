471425fa4b505fa6a1290c4277e905a1eb8acaee
Ghazy <dianghazy@protonmail.com>
2023-02-15 16:40:00 +0700

    add: iriun webcam installer

---
+++ iriun-installer.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo clear
+
+echo Downloading
+
+cd ~/Downloads && wget -r --no-parent -A "iriunwebcam-*.deb" https://iriun.gitlab.io/
+mkdir ~/Downloads/Iriun
+mv ~/Downloads/iriun.gitlab.io/iriunwebcam-*.deb ~/Downloads/Iriun
+cd ~/Downloads/Iriun && ar vx ~/Downloads/Iriun/iriunwebcam-*.deb
+cd ~/Downloads/Iriun && tar -xf ~/Downloads/Iriun/data.tar.xz
+mv ~/Downloads/Iriun/usr/local/bin/iriunwebcam ~/.local/share/bin/
+rm -rf ~/Downloads/Iriun && rm -rf ~/Downloads/iriun.gitlab.io
+
+echo Done, run it by typing iriunwebcam

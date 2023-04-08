97c6635c4c27f64835a1466cc37408defac38fa1
Ghazy <dianghazy@protonmail.com>
2023-04-08 14:05:43 +0700

    update

--- sway/.config/sway/scripts/color_picker.sh
+++ sway/.config/sway/scripts/color_picker.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-picker=$(grim -g "$(slurp -p)" -t ppm - | convert - -format '%[pixel:p{0,0}]' txt:- | tail -n 1 | cut -d ' ' -f 4)
+picker=$(grim -g "$(XCURSOR_THEME=Adwaita XCURSOR_SIZE=10 slurp -p)" -t ppm - | convert - -format '%[pixel:p{0,0}]' txt:- | tail -n 1 | cut -d ' ' -f 4)
 if [ -n "$picker" ]; then
 	tmp=/tmp/${picker}.png
 	echo "$picker" | tr -d "\n" | wl-copy
 	convert -size 64x64 xc:"$picker" "${tmp}"
-	notify-send -t 1500 -u low -r 69 -i "${tmp}" "Copied $picker to clipboard."
+	notify-send -t 3500 -u low -r 69 -i "${tmp}" "$picker"
 	rm "$tmp"
-	exit 0
+	exit 1
 else
-	notify-send -t 1000 -u low -r 69 "Color Picker cancelled."
-	exit 0
+	notify-send -t 3000 -u low -r 69 "Color Picker cancelled."
+	exit 1
 fi
 

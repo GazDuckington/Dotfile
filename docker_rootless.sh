71e7785e2cba74a059f6499e56a5b9923eb7a75d
Ghazy <dianghazy@protonmail.com>
2023-08-22 09:02:40 +0700

    up docker installer

--- docker_rootless.sh
+++ docker_rootless.sh
@@ -5,6 +5,12 @@ if [[ $(id -u) -ne 0 ]]; then
   exit 1
 fi
 
+eccho "install docker rootless"
 systemctl --user stop docker
 rm -f ~/bin/dockerd
 curl -fsSL https://get.docker.com/rootless | sh
+
+echo "install compose plugin"
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose

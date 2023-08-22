3f7334a58df95dc1c9e5832101cf6c5135daa22b
Ghazy <dianghazy@protonmail.com>
2023-08-22 16:19:33 +0700

    idk

--- docker_rootless.sh
+++ docker_rootless.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [[ $(id -u) -ne 0 ]]; then
+if [ "$(id -u)" -ne 0 ]; then
   echo "Please, run as root."
   exit 1
 fi
@@ -12,5 +12,5 @@ curl -fsSL https://get.docker.com/rootless | sh
 
 echo "install compose plugin"
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+mkdir -p "$DOCKER_CONFIG"/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o "$DOCKER_CONFIG"/cli-plugins/docker-compose

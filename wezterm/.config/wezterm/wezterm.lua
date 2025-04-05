bc486917765febdd7e63b2d5d5f42e7f230a2738
Ghazy <dianghazy@protonmail.com>
2025-04-05 11:27:24 +0700

    theming

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,18 +1,20 @@
 local wezterm = require("wezterm")
 
 return {
-	xcursor_theme = 'Vimix-white',
 	default_prog = { '/usr/bin/fish', '-l' },
-	font = wezterm.font 'FiraCode Nerd Font',
-	font_size = 10.0,
-	cursor_blink_rate = 500,
-	window_background_opacity = 0.9,
+	font = wezterm.font {
+		family = 'BlexMono Nerd Font',
+		harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' },
+	},
+	font_size = 12.0,
+	cursor_thickness = 5,
+	window_background_opacity = 0.7,
 	use_fancy_tab_bar = false,
 	tab_bar_at_bottom = true,
 	hide_tab_bar_if_only_one_tab = true,
 	color_scheme = "Catppuccin Mocha",
 	colors = {
-		background = '#11111B',
+		background = '#181825',
 		cursor_bg = '#EC2864',
 		cursor_fg = '#010409'
 	},

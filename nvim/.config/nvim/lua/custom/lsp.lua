29d34db9292fa9b8acf767cacf943978c41e9348
Ghazy <dianghazy@protonmail.com>
2025-06-08 11:29:58 +0700

    fix: lsp config yay

--- nvim/.config/nvim/lua/custom/lsp.lua
+++ nvim/.config/nvim/lua/custom/lsp.lua
@@ -12,34 +12,26 @@ return {
 			"williamboman/mason-lspconfig.nvim",
 			"nvim-treesitter/nvim-treesitter",
 		},
+		config = function()
+			local vue_ls_path = vim.fn.expand("$MASON/packages/vue-language-server")
+			local vue_plugin_path = vue_ls_path .. "/node_modules/@vue/language-server"
+			local vue_ts_plugin = vue_ls_path .. "/node_modules/typescript/lib"
 
-		opts = {
-			servers = {
-				-- Lua LSP
-				lua_ls = {
-					settings = {
-						Lua = {
-							diagnostics = { globals = { "vim" } },
-							workspace = {
-								library = vim.api.nvim_get_runtime_file("", true),
-								userThirdParty = { os.getenv("HOME") .. ".local/share/lua-addons" },
-								checkThirdParty = "Apply"
-							},
+			vim.lsp.config('lua_ls', {
+				settings = {
+					Lua = {
+						runtime = { version = 'Lua 5.1' },
+						diagnostics = {
+							globals = { 'bit', 'vim', 'it', 'describe', 'before_each', 'after_each' },
+						},
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true),
+							userThirdParty = { os.getenv("HOME") .. ".local/share/lua-addons" },
+							checkThirdParty = "Apply"
 						},
 					},
 				},
-			}
-		},
-
-		config = function(_, opts)
-			local lspconfig = require('lspconfig')
-			local capabilities = require('blink.cmp').get_lsp_capabilities()
-
-			for server, config in pairs(opts.servers) do
-				config.capabilities = capabilities
-				lspconfig[server].setup(config)
-			end
-
+			})
 			vim.lsp.config('vue_ls', {
 				-- add filetypes for typescript, javascript and vue
 				filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
@@ -49,9 +41,10 @@ return {
 					-- hybridMode = false,
 					-- },
 					typescript = {
-						tsdk = vim.fn.expand(
-							"~/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib"
-						),
+						-- tsdk = vim.fn.expand(
+						-- 	"~/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib"
+						-- ),
+						tsdk = vue_ts_plugin,
 					},
 				},
 				before_init = function(params, config)
@@ -67,13 +60,16 @@ return {
 					plugins = {
 						{
 							name = "@vue/typescript-plugin",
-							location =
-							"~/.local/share/nvim/mason/packages/vue-language-server/node_modules/@vue/language-server",
-							languages = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+							location = vue_plugin_path,
+							languages = { 'typescript', 'javascript', 'vue' },
 						},
 					},
 				},
-				filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+				filetypes = {
+					'javascript',
+					'typescript',
+					'vue',
+				},
 			})
 		end
 	}

ea11fbedf90492e69639abe4e07caeaa59c4ff57
Ghazy <dianghazy@protonmail.com>
2025-08-02 17:30:32 +0700

    jhfgjh

--- hypr/.config/hypr/scripts/currentsong.sh
+++ hypr/.config/hypr/scripts/currentsong.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "  $(playerctl metadata --format '{{artist}}-{{title}}' 2>/dev/null || mpc --format '%artist%-%title%' current)"
+echo "$(playerctl metadata --format '{{artist}}-{{title}}' 2>/dev/null || mpc --format '%artist%-%title%' current)"

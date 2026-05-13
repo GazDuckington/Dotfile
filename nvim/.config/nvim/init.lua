682538025bec4413417136e04f65346941e79746
Ghazy <dianghazy@protonmail.com>
2026-05-13 15:46:17 +0700

    disable netrw

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,6 +7,9 @@
 -- https://gtihub.com/GazDuckington/Dotfile/nvim
 --
 vim.g.mapleader = " "
-vim.pack.add({"https://github.com/nvim-tree/nvim-web-devicons"})
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.pack.add({ "https://github.com/nvim-tree/nvim-web-devicons" })
 require("settings").setup()
 require("keybinds").setup()

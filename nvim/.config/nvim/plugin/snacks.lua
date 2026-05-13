d7156d00a4fe58664d7b5614b72b2b38a6a6a738
Ghazy <dianghazy@protonmail.com>
2026-05-13 15:44:25 +0700

    dash

--- nvim/.config/nvim/plugin/snacks.lua
+++ nvim/.config/nvim/plugin/snacks.lua
@@ -6,7 +6,27 @@ require("snacks").setup({
 	statuscolumn = { enabled = true },
 	words = { enabled = true },
 	dashboard = {
-		enabled = false,
+		enabled = true,
+		sections = {
+			{ section = "header" },
+			{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+			{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+			{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+			{
+				icon = " ",
+				title = "System",
+				desc = "Packages Loaded",
+				padding = 1,
+				indent = 2,
+				-- Use the 'text' key with a generated string instead of a raw function
+				text = {
+					{
+						"󰚥 " .. #vim.api.nvim_list_runtime_paths() .. " packages in runtime",
+						hl = "SnacksDashboardSpecial",
+					},
+				},
+			},
+		},
 	},
 	dim = { enabled = true },
 	zen = { enabled = true },

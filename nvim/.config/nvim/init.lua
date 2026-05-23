2b50899ecd3c2de8df41b838f3130babe9268cec
Ghazy <dianghazy@protonmail.com>
2026-05-23 12:56:06 +0700

    add: plkenary

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -11,5 +11,6 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.pack.add({ "https://github.com/nvim-tree/nvim-web-devicons" })
+vim.pack.add({ "https://github.com/nvim-lua/plenary.nvim" })
 require("settings").setup()
 require("keybinds").setup()

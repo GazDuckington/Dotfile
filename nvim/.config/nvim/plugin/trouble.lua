2cc9291f4aa25c26feb6688206435e7607d8a76a
Ghazy <dianghazy@protonmail.com>
2026-06-22 09:00:49 +0700

    add: trouble.nvim

---
+++ nvim/.config/nvim/plugin/trouble.lua
@@ -0,0 +1,3 @@
+vim.pack.add({ "https://github.com/folke/trouble.nvim" })
+vim.cmd("packadd trouble.nvim")
+require("trouble").setup({})
--- nvim/.config/nvim/plugin/whichkey.lua
+++ nvim/.config/nvim/plugin/whichkey.lua
@@ -57,16 +57,32 @@ local mappings = {
 	{ "<leader>x", group = "Trouble" },
 	{
 		"<leader>xx",
-		function()
-			Snacks.picker.diagnostics()
-		end,
+		"<cmd>Trouble diagnostics toggle<cr>",
 		desc = "Workspace Diagnostics",
 	},
 	{
+		"<leader>xX",
+		"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+		desc = "Buffer Diagnostics (Trouble)",
+	},
+	{
+		"<leader>cs",
+		"<cmd>Trouble symbols toggle focus=false<cr>",
+		desc = "Symbols (Trouble)",
+	},
+	{
+		"<leader>cl",
+		"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+		desc = "LSP Definitions / references / ... (Trouble)",
+	},
+	{
+		"<leader>xL",
+		"<cmd>Trouble loclist toggle<cr>",
+		desc = "Location List (Trouble)",
+	},
+	{
 		"<leader>xq",
-		function()
-			Snacks.picker.qflist()
-		end,
+		"<cmd>Trouble qflist toggle<cr>",
 		desc = "QuickFix",
 	},
 

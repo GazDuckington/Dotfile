c3a43e0f83f18576769569205162030551a8ee96
Ghazy <dianghazy@protonmail.com>
2026-09-18 10:30:24 +0700

    up

--- hypr/.config/hypr/conf/keybinding.lua
+++ hypr/.config/hypr/conf/keybinding.lua
@@ -8,55 +8,55 @@ local d = hl.dsp
 -- Structure: { Modifiers, Key, Dispatcher Object }
 M.binds = {
 	-- General Actions
-	{ main_mod,             "F4",        d.exec_cmd("noctalia msg panel-toggle session") },
-	{ main_mod,             "Z",         d.exec_cmd("~/.local/bin/kitty-launch.sh wiremix") },
-	{ main_mod,             "Return",    d.exec_cmd(terminal) },
-	{ main_mod .. " SHIFT", "Return",    d.exec_cmd("noctalia msg panel-toggle wallpaper") },
-	{ main_mod,             "Q",         d.window.close() },
-	{ main_mod,             "E",         d.exec_cmd("dolphin") },
-	{ main_mod,             "F",         d.window.float({ action = "toggle" }) },
-	{ "CTRL",               "SPACE",     d.exec_cmd("noctalia msg panel-toggle launcher") },
-	{ main_mod,             "C",         d.exec_cmd("bash -c '~/.local/bin/color_picker.sh'") },
-	{ main_mod,             "D",         d.exec_cmd("noctalia msg panel-toggle control-center") },
+	{ main_mod, "F4", d.exec_cmd("noctalia msg panel-toggle session") },
+	{ main_mod, "Z", d.exec_cmd("~/.local/bin/kitty-launch.sh wiremix") },
+	{ main_mod, "Return", d.exec_cmd(terminal) },
+	{ main_mod .. " SHIFT", "Return", d.exec_cmd("noctalia msg panel-toggle wallpaper") },
+	{ main_mod, "Q", d.window.close() },
+	{ main_mod, "E", d.exec_cmd("dolphin") },
+	{ main_mod, "F", d.window.float({ action = "toggle" }) },
+	{ "CTRL", "SPACE", d.exec_cmd("noctalia msg panel-toggle launcher") },
+	{ main_mod, "C", d.exec_cmd("bash -c '~/.local/bin/color_picker.sh'") },
+	{ main_mod, "D", d.exec_cmd("noctalia msg panel-toggle control-center") },
 	-- { main_mod .. " SHIFT", "S", d.exec_cmd("noctalia msg settings-toggle") },
-	{ main_mod .. " ALT",   "L",         d.exec_cmd("noctalia msg session lock") },
+	{ main_mod .. " ALT", "L", d.exec_cmd("noctalia msg session lock") },
 
 	-- Layout & Master
-	{ main_mod,             "M",         d.window.fullscreen(1) },
-	{ main_mod .. " SHIFT", "SPACE",     d.layout("swapwithmaster master") },
-	{ main_mod .. " SHIFT", "F",         d.window.fullscreen() },
-	{ main_mod,             "SPACE",     d.layout("focusmaster") },
-	{ "ALT",                "SPACE",     d.exec_cmd("sh ~/.config/hypr/scripts/focusfloat.sh") },
+	{ main_mod, "W", d.layout("colresize 1.0") },
+	{ main_mod .. " SHIFT", "SPACE", d.layout("swapwithmaster master") },
+	{ main_mod .. " SHIFT", "F", d.window.fullscreen() },
+	{ main_mod, "SPACE", d.layout("focusmaster") },
+	{ "ALT", "SPACE", d.exec_cmd("sh ~/.config/hypr/scripts/focusfloat.sh") },
 
 	-- Focus movement
-	{ main_mod,             "h",         d.focus({ direction = "left" }) },
-	{ main_mod,             "l",         d.focus({ direction = "right" }) },
-	{ main_mod,             "k",         d.focus({ direction = "up" }) },
-	{ main_mod,             "j",         d.focus({ direction = "down" }) },
+	{ main_mod, "h", d.focus({ direction = "left" }) },
+	{ main_mod, "l", d.focus({ direction = "right" }) },
+	{ main_mod, "k", d.focus({ direction = "up" }) },
+	{ main_mod, "j", d.focus({ direction = "down" }) },
 
 	-- Move windows
-	{ main_mod .. " SHIFT", "H",         d.window.move({ direction = "left" }) },
-	{ main_mod .. " SHIFT", "L",         d.window.move({ direction = "right" }) },
-	{ main_mod .. " SHIFT", "K",         d.window.move({ direction = "up" }) },
-	{ main_mod .. " SHIFT", "J",         d.window.move({ direction = "down" }) },
+	{ main_mod .. " SHIFT", "H", d.window.move({ direction = "left" }) },
+	{ main_mod .. " SHIFT", "L", d.window.move({ direction = "right" }) },
+	{ main_mod .. " SHIFT", "K", d.window.move({ direction = "up" }) },
+	{ main_mod .. " SHIFT", "J", d.window.move({ direction = "down" }) },
 
 	-- Workspace Navigation
-	{ main_mod,             "TAB",       d.focus({ workspace = "e+1" }) },
-	{ main_mod .. " SHIFT", "TAB",       d.focus({ workspace = "e-1" }) },
+	{ main_mod, "TAB", d.focus({ workspace = "e+1" }) },
+	{ main_mod .. " SHIFT", "TAB", d.focus({ workspace = "e-1" }) },
 
 	-- Special Workspaces
-	{ main_mod,             "S",         d.workspace.toggle_special("magic") },
-	{ main_mod .. " SHIFT", "S",         d.window.move({ workspace = "special:magic" }) },
+	{ main_mod, "S", d.workspace.toggle_special("magic") },
+	{ main_mod .. " SHIFT", "S", d.window.move({ workspace = "special:magic" }) },
 
 	-- Utilities & Screenshots
-	{ main_mod,             "V",         d.exec_cmd("noctalia msg panel-toggle clipboard") },
-	{ "",                   "PRINT",     d.exec_cmd("noctalia msg screenshot-fullscreen all") },
-	{ "SHIFT",              "PRINT",     d.exec_cmd("noctalia msg screenshot-region") },
-	{ "CTRL SHIFT",         "PRINT",     d.exec_cmd("noctalia msg screenshot-fullscreen pick") },
+	{ main_mod, "V", d.exec_cmd("noctalia msg panel-toggle clipboard") },
+	{ "", "PRINT", d.exec_cmd("noctalia msg screenshot-fullscreen all") },
+	{ "SHIFT", "PRINT", d.exec_cmd("noctalia msg screenshot-region") },
+	{ "CTRL SHIFT", "PRINT", d.exec_cmd("noctalia msg screenshot-fullscreen pick") },
 
 	-- Mouse
-	{ main_mod,             "mouse:272", d.window.drag() },
-	{ main_mod,             "mouse:273", d.window.resize() },
+	{ main_mod, "mouse:272", d.window.drag() },
+	{ main_mod, "mouse:273", d.window.resize() },
 }
 
 -- Repeating Binds (Workspaces 1-10)
@@ -77,15 +77,15 @@ M.binde = {
 
 -- Media Keys (Locked 'l' flag)
 M.bindl = {
-	{ "", "XF86AudioRaiseVolume",  d.exec_cmd("noctalia msg volume-up") },
-	{ "", "XF86AudioLowerVolume",  d.exec_cmd("noctalia msg volume-down") },
-	{ "", "XF86AudioMute",         d.exec_cmd("noctalia msg volume-mute") },
-	{ "", "XF86AudioMicMute",      d.exec_cmd("noctalia msg mic-mute") },
-	{ "", "XF86AudioNext",         d.exec_cmd("noctalia msg media next") },
-	{ "", "XF86AudioPrev",         d.exec_cmd("noctalia msg media previous") },
-	{ "", "XF86AudioPlay",         d.exec_cmd("noctalia msg media play") },
-	{ "", "XF86AudioPause",        d.exec_cmd("noctalia msg media stop") },
-	{ "", "XF86MonBrightnessUp",   d.exec_cmd("noctalia msg brightness-up") },
+	{ "", "XF86AudioRaiseVolume", d.exec_cmd("noctalia msg volume-up") },
+	{ "", "XF86AudioLowerVolume", d.exec_cmd("noctalia msg volume-down") },
+	{ "", "XF86AudioMute", d.exec_cmd("noctalia msg volume-mute") },
+	{ "", "XF86AudioMicMute", d.exec_cmd("noctalia msg mic-mute") },
+	{ "", "XF86AudioNext", d.exec_cmd("noctalia msg media next") },
+	{ "", "XF86AudioPrev", d.exec_cmd("noctalia msg media previous") },
+	{ "", "XF86AudioPlay", d.exec_cmd("noctalia msg media play") },
+	{ "", "XF86AudioPause", d.exec_cmd("noctalia msg media stop") },
+	{ "", "XF86MonBrightnessUp", d.exec_cmd("noctalia msg brightness-up") },
 	{ "", "XF86MonBrightnessDown", d.exec_cmd("noctalia msg brightness-down") },
 }
 
